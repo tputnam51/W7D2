eb47a7e00aede499f7f20759ed6a7d9bb786b397
Travis <tputnam8170@gmail.com>
2021-04-06 17:21:54 -0400

    Bands controller

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,4 +1,54 @@
 class BandsController < ApplicationController
 
-    
+    def index
+        @band = Band.all
+        render :index
+    end
+
+    def create
+        @band = Band.create(band_params)
+
+        if band.save!
+            redirect_to band_url(@band)
+        else 
+            flash.new[:errors] = @band.errors.full_messages
+            render :new
+        end
+    end
+
+    def new
+        @band = Band.new
+        render :new
+    end
+
+    def edit
+        @band = Band.find(params[:id])
+        render :edit
+    end
+
+    def show
+        @band = Band.find(params[:id])
+        render :show
+    end
+
+    def update
+        @band = Band.find(params[:id])
+
+        if @band.update(band_params)
+            redirect_to band_url
+        else
+            flash.now[:errors] = @band.errors.full_messages
+            render :edit
+        end
+    end
+
+    def destroy
+        @band = Band.find(params[:id])
+        @band.destroy
+        redirect_to bands_url
+    end
+
+    def band_params
+        params.require(:band).permit(:name)
+    end
 end
